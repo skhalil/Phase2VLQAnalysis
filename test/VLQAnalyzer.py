ab35f762e60d371c0e3deffd82afd61cf35337c8
gaseoussynonyms <zolson956@gmail.com>
2018-05-24 16:46:21 -0500

    Added b-tagging (deepcsvm) and higgs tagging. Created the mass reconstruction function

--- test/VLQAnalyzer.py
+++ test/VLQAnalyzer.py
@@ -22,7 +22,241 @@ def Overlaps2D(jetsP4, lepP4, drMax, ptrelMin):
          return overlaps
    print 'overlap ?? ', overlaps
    return overlaps 
-      
+
+def SolveNuPz(vlep, vnu, wmass, nuz1, nuz2):
+    solutionBool = True
+    x = vlep.X()*vnu.X() + vlep.Y()*vnu.Y() + wmass*wmass/2
+    a = vlep.Z()*vlep.Z() - vlep.E()*vlep.E()
+    b = 2*x*vlep.Z()
+    c = x*x - vnu.Perp2() * vlep.E()*vlep.E()
+    d = b*b - 4*a*c
+    #print 'x: ', x
+    #print 'a: ', a
+    #print 'b: ', b
+    #print 'c: ', c
+    #print 'd: ', d
+    if d < 0:
+        d = 0
+        solutionBool = False
+    nuz1 = (-b + np.sqrt(d))/2/a
+    nuz2 = (-b -np.sqrt(d))/2/a
+    #print 'nuz1: ', nuz1
+    #print 'nuz2: ', nuz2
+    if abs(nuz1) > abs(nuz2):
+        lowsol = nuz2
+        highsol = nuz1
+    else:
+        lowsol = nuz1
+	highsol = nuz2
+    return solutionBool, lowsol, highsol;
+
+def AdjustEnergyForMass(v, mass):
+    v.SetE(np.sqrt(v.Vect().Mag2() + mass*mass))
+    return;
+
+def nextPerm(lis):
+    n = len(lis)
+
+    i = n-2
+    while i >= 0 and lis[i] >= lis[i+1]:
+	i -= 1
+
+    if i == -1:
+	return False;
+
+    j = i + 1
+    while j < n and lis[j] > lis[i]:
+	j += 1
+    j -= 1
+
+    lis[i], lis[j] = lis[j], lis[i]
+
+    left = i + 1
+    right = n -1
+
+    while left < right:
+	lis[left], lis[right] = lis[right], lis[left]
+	left += 1
+	right -= 1
+
+    return True;
+
+def GetChi2(JetsP4, LeptonP4, NuP4, topMass, higgsMass, topP4, higgsP4, dR):
+    top = 0.0; higgs = 0.0; top_chi2 = 0.0; higgs_chi2 = 0.0; dR_topH = 0.0; dR_topH_chi2 = 0.0
+    top = abs( (JetsP4[0] + LeptonP4 + NuP4).M() - topMass)
+    top_chi2 = top*top / (14.5*14.5) #where did 14.5 come from?
+    topP4 = JetsP4[0] + LeptonP4 + NuP4
+
+    higgs = abs((JetsP4[1] + JetsP4[2]).M() - higgsMass)
+    higgs_chi2 = higgs*higgs / (14.5*14.5) #again, where did 14.5 come from?
+    higgsP4 = JetsP4[1] + JetsP4[2]
+
+    dR_topH = abs( topP4.DeltaR(higgsP4) - 3.15) #3.15
+    dR_topH_chi2 = dR_topH*dR_topH / (0.196*0.196) #0.196
+    dR = topP4.DeltaR(higgsP4)
+
+    if JetsP4[0].DeltaR(higgsP4) < 1.0:
+	return 100000.0, 100000.0;
+    else:
+	return top_chi2 + higgs_chi2 + dR_topH_chi2, dR
+    
+def GetChi2Boosted(ak4jetsP4, higgsJet, higgsSoftDropMass, leptonP4, nuP4, topMass, higgsMass, topP4, higgsP4, dR):
+    top = 0.0; higgs = 0.0; top_chi2 = 0.0; higgs_chi2 = 0.0; dR_topH = 0.0; dR_topH_chi2 = 0.0
+    
+    top = abs( (ak4jetsP4[0] + leptonP4 + nuP4).M() - topMass)
+    top_chi2 = top*top / (14.5*14.5)
+    topP4 = ak4jetsP4[0] + leptonP4 + nuP4
+
+    higgs = abs(higgsSoftDropMass[0] - higgsMass)
+    higgs_chi2 = higgs*higgs / (14.5*14.5)
+    higgsP4 = higgsJet[0]
+
+    dR_topH = abs(topP4.DeltaR(higgsP4) - 3.15)
+    dR_topH_chi2 = dR_topH*dR_topH / (0.196*0.196)
+    #print 'infuncdRBefore: ', dR
+    dR = topP4.DeltaR(higgsP4)
+    #print 'infuncdRAfter: ', dR
+
+    if ak4jetsP4[0].DeltaR(higgsP4) < 1.0:
+	return 100000.0, 100000.0, TLorentzVector(100000.0, 100000.0, 100000.0, 100000.0), TLorentzVector(100000.0, 100000.0, 100000.0, 100000.0)
+    else:
+	return top_chi2 + higgs_chi2 + dR_topH_chi2, dR, topP4, higgsP4
+ 
+def DoMassReco(jetColl, LeptonP4, NuP4, higgsMass, topMass, chi2_dR1, chi2_dR2, chi2_higgs1, chi2_higgs2, chi2_top1, chi2_top2):
+    chi2_higgs1 = 100000.0; chi2_top1 = 100000.0
+    JetsP4 = []
+    index_list = [0, 1, 2, 3]
+    index_list1 = [0, 1, 2]
+
+    chi2 = 100000.0; dR = 100000.0; minChi2 = 100000.0
+
+    topP4 = TLorentzVector(0.0, 0.0, 0.0, 0.0)
+    higgsP4 = TLorentzVector(0.0, 0.0, 0.0, 0.0)
+
+    if len(jetColl) > 3:
+	while True:
+	    i0 = index_list[0]
+	    i1 = index_list[1]
+	    i2 = index_list[2]
+    	    i3 = index_list[3]
+
+	    jetsP4PassToChi2 = []
+
+	    if len(jetsP4PassToChi2) != 0:
+	        jetsP4PassToChi2.clear()
+	    topP4.Clear()
+	    higgsP4.Clear()
+	    JetsP4 = []
+
+	    JetsP4.append(jetColl[i0])
+	    JetsP4.append(jetColl[i1])
+	    JetsP4.append(jetColl[i2])
+	    JetsP4.append(jetColl[i3])
+	    jetsP4PassToChi2.append(JetsP4[0])
+	    jetsP4PassToChi2.append(JetsP4[1])
+	    jetsP4PassToChi2.append(JetsP4[2])
+	    jetsP4PassToChi2.append(JetsP4[3])
+
+	    chi2, dR = GetChi2(jetsP4PassToChi2, LeptonP4, NuP4, topMass, higgsMass, topP4, higgsP4, dR)
+
+	    if (chi2 < minChi2):
+	        minChi2 = chi2
+	    
+	        chi2_higgs1 = minChi2
+	        chi2_higgs2 = higgsP4
+
+	        chi2_top1 = minChi2
+	        chi2_top2 = topP4
+
+	        chi2_dR1 = minChi2
+	        chi2_dR2 = dR
+	    if not nextPerm(index_list):
+	        break
+    elif len(jetColl) == 3:
+	while True:
+	    i0 = index_list[0]
+	    i1 = index_list[1]
+	    i2 = index_list[2]
+
+	    jetsP4PassToChi2 = []
+
+	    if len(jetsP4PassToChi2) != 0:
+		jetsP4PassToChi2.clear()
+	    topP4.Clear()
+	    higgsP4.Clear()
+	    JetsP4 = []
+
+	    JetsP4.append(jetColl[i0])
+            JetsP4.append(jetColl[i1])
+            JetsP4.append(jetColl[i2])
+
+	    jetsP4PassToChi2.append(JetsP4[0])
+	    jetsP4PassToChi2.append(JetsP4[1])
+	    jetsP4PassToChi2.append(JetsP4[2])
+
+	    chi2, dR = GetChi2(jetsP4PassToChi2, LeptonP4, NuP4, topMass, higgsMass, topP4, higgsP4, dR)
+
+	    if chi2 < minChi2:
+		minChi2 = chi2
+		
+		chi2_higgs1 = minChi2
+		chi2_higgs2 = higgsP4
+
+		chi2_top1 = minChi2
+		chi2_top2 = topP4
+
+		chi2_dR1 = minChi2
+		chi2_dR2 = dR
+	    if not nextPerm(index_list1):
+                break
+
+    return chi2_higgs1, chi2_higgs2, chi2_top2, chi2_dR2;
+
+def DoMassRecoBoost(ak4Jets, higgsJet, higgsSoftDropMass, leptonP4, nuP4, higgsMass, topMass, chi2_dR1, chi2_dR2, chi2_higgs1, chi2_higgs2, chi2_top1, chi2_top2):
+    ak4JetsP4 = [TLorentzVector(0.0, 0.0, 0.0, 0.0), TLorentzVector(0.0, 0.0, 0.0, 0.0)]
+    index_list = [0, 1]
+    chi2 = 100000.0; dR = 100000.0; minChi2 = 100000.0
+    topP4 = TLorentzVector(0.0, 0.0, 0.0, 0.0)
+    higgsP4 = TLorentzVector(0.0, 0.0, 0.0, 0.0)
+
+    if len(higgsJet) > 0:
+	while True:
+	    i0 = index_list[0]
+	    i1 = index_list[1]
+	    jetsP4PassToChi2 = []
+
+	    if len(jetsP4PassToChi2) != 0:
+                jetsP4PassToChi2.clear()
+	    topP4.Clear()
+	    higgsP4.Clear()
+
+	    ak4JetsP4[0] = ak4Jets[i0]
+	    if len(ak4Jets) > 1:
+		ak4JetsP4[1] = ak4Jets[i1]
+	    else:
+		ak4JetsP4[1].SetPtEtaPhiM(0.0, 0.0, 0.0, 0.0)
+
+	    jetsP4PassToChi2.append(ak4Jets[i0])
+	    jetsP4PassToChi2.append(ak4Jets[i1])
+
+            chi2, dR, topP4, higgsP4 = GetChi2Boosted(jetsP4PassToChi2, higgsJet, higgsSoftDropMass, leptonP4, nuP4, topMass, higgsMass, topP4, higgsP4, dR)
+
+	    if chi2 < minChi2:
+		minChi2 = chi2
+
+		chi2_higgs1 = minChi2
+		chi2_higgs2 = higgsP4
+
+		chi2_top1 = minChi2
+		chi2_top2 = topP4
+
+		chi2_dR1 = minChi2
+		chi2_dR2 = dR		
+	    if not nextPerm(index_list):
+                break
+
+    return chi2_higgs1, chi2_higgs2, chi2_top2, chi2_dR2
+
 from optparse import OptionParser
 
 # Create a command line option parser
@@ -83,6 +317,12 @@ hLeadingJetEta = TH1D("hLeadingJetEta", "Leading Jet #eta; #eta; Events;", 80, -
 hNCentJets = TH1D("hNCentJets", "Number of Central Jets; Number of Central Jets; Events;", 40, 0, 40)
 hSecJetPt = TH1D("hSecJetPt", "Second Leading Jet p_{T}; p_{T} {GeV}; Events;", 50, 0, 1000)
 hSecJetEta = TH1D("hSecJetEta", "Second Leading Jet #eta; #eta; Events;", 80, -4.0, 4.0)
+hLargestDeepCSVB = TH1D("hLargestDeepCSVB", "Largest B-Tag Value from DeepCSV; B-Tag Value; Events;", 500, 0, 1.5)
+hMET = TH1D("hMET", "Missing E_{T}; MET {GeV}; Events;", 50, 0, 1000)
+hak8JetPt = TH1D("hak8JetPt", "ak8Jet p_{T}; p_{T} {GeV}; Events;", 50, 0, 1000)
+hak8JetEta = TH1D("hak8JetEta", "ak8Jet #eta; #eta; Events;", 80, -4.0, 4.0)
+hhiggsJetPt = TH1D("hhiggsJetPt", "higgsJet p_{T}; p_{T} {GeV}; Events;", 50, 0, 1000)
+hhiggsJetEta = TH1D("hhiggsJetEta", "higgsJet #eta; #eta; Events;", 80, -4.0, 4.0)
 
 lepP4        = TLorentzVector(0.0, 0.0, 0.0, 0.0)
 jetP4        = TLorentzVector(0.0, 0.0, 0.0, 0.0)
@@ -192,7 +432,7 @@ for fname in fnames:
       hjetsPt.Fill(ak4jet_pt[j], evtwt)
       hjetsEta.Fill(ak4jet_eta[j], evtwt)
       if ak4jet_pt[j] < 30. : continue
-      if ak4jet_eta[j] > 5.0 : continue
+      if abs(ak4jet_eta[j]) > 5.0 : continue
       jetP4 = TLorentzVector(0.0, 0.0, 0.0, 0.0)
       jetP4.SetPtEtaPhiM(ak4jet_pt[j], ak4jet_eta[j], ak4jet_phi[j], ak4jet_m[j])     
       #print 'what is going inside: ', jetP4.Pt()
@@ -246,7 +486,10 @@ for fname in fnames:
 	    centjets.append(jet1)
 	else:
 	    fjets.append(jet1)
-    
+ 
+    #print 'Length of good jets: ', len(jetsP4)
+    #print 'Length of Btag list: ', len(jetsBtag)
+
     # histograms showing number of forward and central jets before jet cuts
     hNForwardJets.Fill(len(fjets), evtwt)
     hNCentJets.Fill(len(centjets), evtwt)
@@ -259,6 +502,8 @@ for fname in fnames:
     if len(centjets) < 3: continue
     ncut += 1
     hCutflow.Fill(ncut, evtwt)
+
+    if centjets[0].Pt() < centjets[1].Pt(): print 'not ordered properly'
 
     # leading jet pt > 200
     leadjet = centjets[0]
@@ -281,11 +526,153 @@ for fname in fnames:
     hLeadingJetEta.Fill(leadjeteta, evtwt)
     hSecJetPt.Fill(secondjetpt, evtwt)
     hSecJetEta.Fill(secondjeteta, evtwt)
+   
+    # cut out events without 1 B jet (using deepcsvm value) 
+    ak4jet_deepcsv = t.AK4JetsCHS_deepcsv
+    ngoodjets = len(jetsP4)
+    #print 'ngoodjets: ', ngoodjets
+    jetsBtag = []
+    deepcsvm = 0.4941
+    nMedBjets = 0
+    largestB = 0
+    for j in range(0, ngoodjets):
+	jetsBtag.append(ak4jet_deepcsv[j])
+    nbseljets = len(jetsBtag)
+    #print 'nbseljets: ', nbseljets
+    for j in jetsBtag:
+        #print 'Btag values before cut: ', j
+        if j < deepcsvm: continue
+        nMedBjets += 1
+	#print 'Btag values after cut: ', j
+        if j > largestB:
+	    largestB = j
+    if nMedBjets == 0: continue
+    ncut += 1
+    hCutflow.Fill(ncut, evtwt)
+    #print 'largestB: ', largestB
+    hLargestDeepCSVB.Fill(largestB, evtwt)
+
+    # Met > 20 GeV
+    met_pt = t.MET_pt
+    met_px = t.MET_px
+    met_py = t.MET_py
+    met_pz = t.MET_pz
+    if met_pt <= 20: continue
+    ncut += 1
+    hCutflow.Fill(ncut, evtwt)
+    hMET.Fill(met_pt, evtwt)
+
+    #print 'met_px: ', met_px
+    #print 'met_py: ', met_py
+    #print 'met_pz: ', met_pz
+ 
+    # Higgs Tagging
+    ak8jet_pt = t.AK8Jets_ptCHS
+    ak8jet_eta = t.AK8Jets_etaCHS
+    ak8jet_phi = t.AK8Jets_phiCHS
+    ak8jet_m = t.AK8Jets_massCHS
+    ak8jet_tau1 = t.AK8Jets_tau1CHS
+    ak8jet_tau2 = t.AK8Jets_tau2CHS
+    ak8jet_sj1pt = t.AK8Jets_sj0pt
+    ak8jet_sj2pt = t.AK8Jets_sj1pt
+    ak8jet_sdmass = t.AK8Jets_softDropMassCHS
+    ak8jet_sj1deepcsv = t.AK8Jets_sj0deepcsv
+    ak8jet_sj2deepcsv = t.AK8Jets_sj1deepcsv
+    nak8jet = len(ak8jet_pt)
+    nHiggs = 0
+
+    ak8jetsP4 = []
+    higgsjets = []
+    higgsSoftDropM = []
+
+    for j in range(0, nak8jet):
+	# fill histos with ak8pt and ak8eta
+	hak8JetPt.Fill(ak8jet_pt[j], evtwt)
+	hak8JetEta.Fill(ak8jet_eta[j], evtwt)
+	#print 'ak8 tau2: ', ak8jet_tau2[j]
+	#print 'ak8 tau1: ', ak8jet_tau1[j]
+	#print 'subjettiness: ', ak8jet_tau2[j]/ak8jet_tau1[j]
+	#print 'length of subjet1: ', len(ak8jet_sj1pt)
+	#print 'length of subjet2: ', len(ak8jet_sj2pt)
+	if ak8jet_pt[j] < 300.0: continue
+	if abs(ak8jet_eta[j]) > 2.4: continue
+	if (ak8jet_tau2[j]/ak8jet_tau1[j]) > 0.6: continue
+	if len(ak8jet_sj1pt) == 0 and len(ak8jet_sj2pt) == 0: continue
+	ak8jetP4 = TLorentzVector(0.0, 0.0, 0.0, 0.0)
+	ak8jetP4.SetPtEtaPhiM(ak8jet_pt[j], ak8jet_eta[j], ak8jet_phi[j], ak8jet_m[j])
+	#print 'still pt here: ', ak8jetP4.Pt()
+	#print 'deltaR: ', ak8jetP4.DeltaR(lepP4)
+	#print 'softdropmass: ', ak8jet_sdmass[j]
+	if ak8jetP4.DeltaR(lepP4) <= 1.0: continue
+	if ak8jet_sdmass[j] > 160 or ak8jet_sdmass[j] < 90: continue
+       
+	# b tagging of subjets
+	if ak8jet_sj1deepcsv[j] < deepcsvm: continue
+	if ak8jet_sj2deepcsv[j] < deepcsvm: continue
+	#print 'subjet1 deepcsv: ', ak8jet_sj1deepcsv[j]
+	#print 'subjet2 deepcsv: ', ak8jet_sj2deepcsv[j]
+ 
+	nHiggs += 1
+
+	higgsjets.append(ak8jetP4)
+	higgsSoftDropM.append(ak8jet_sdmass[j])
+	hhiggsJetPt.Fill(ak8jetP4.Pt(), evtwt)
+	hhiggsJetEta.Fill(ak8jetP4.Eta(), evtwt)
+
+    if nHiggs == 0: continue
+    ncut += 1
+    hCutflow.Fill(ncut, evtwt)
+
+    # Top mass reconstruction
+    nuP4 = TLorentzVector(met_px, met_py, met_pz, 0.0)
+    lowersol = 0.0; highersol = 0.0
+    isNuPz, lowersol, highersol = SolveNuPz(lepP4, nuP4, 80.4, lowersol, highersol)
+
+    #print 'sollow: ', lowersol
+    #print 'solhigh: ', highersol
+    #print 'isNuPz: ', isNuPz
+
+    nuP4.SetPz(lowersol)
+    #print 'nuP4before: ', nuP4.Px(), nuP4.Py(), nuP4.Pz(), nuP4.E()
+    AdjustEnergyForMass(nuP4, 0.0)
+    #print 'nuP4after: ', nuP4.Px(), nuP4.Py(), nuP4.Pz(), nuP4.E()
+
+    topMass = 174.0; higgsMass = 125.0; chi2_dR1 = 0.0; chi2_dR2 = 100000.0; chi2_higgs1 = 0.0; chi2_top1 = 0.0; chi2_dR_boost1 = 0.0; chi2_dR_boost2 = 0.0; chi2_higgs_boost1 = 0.0; chi2_top_boost1 = 0.0
+    chi2_higgs2 = TLorentzVector(0.0, 0.0, 0.0, 0.0)
+    chi2_top2 = TLorentzVector(0.0, 0.0, 0.0, 0.0)
+    chi2_higgs_boost2 = TLorentzVector(0.0, 0.0, 0.0, 0.0)
+    chi2_top_boost2 = TLorentzVector(0.0, 0.0, 0.0, 0.0)
+    higgsP4 = TLorentzVector(0.0, 0.0, 0.0, 0.0)
+    topP4 = TLorentzVector(0.0, 0.0, 0.0, 0.0)
+    chi2 = 100000.0
+
+    print 'beforechi2: ', chi2
+    print 'beforedR: ', dR
+    print 'beforetopP4: ', topP4.Px(), topP4.Py(), topP4.Pz(), topP4.E()
+    print 'beforehiggsP4: ', higgsP4.Px(), higgsP4.Py(), higgsP4.Pz(), higgsP4.E()
+ 
+    chi2, higgsP4, topP4, dR = DoMassRecoBoost(centjets, higgsjets, higgsSoftDropM, lepP4, nuP4, higgsMass, topMass, chi2_dR_boost1, chi2_dR_boost2, chi2_higgs_boost1, chi2_higgs_boost2, chi2_top_boost1, chi2_top_boost2)
+
+    print 'afterchi2: ', chi2
+    print 'afterdR: ', dR
+    print 'aftertopP4: ', topP4.Px(), topP4.Py(), topP4.Pz(), topP4.E()
+    print 'afterhiggsP4: ', higgsP4.Px(), higgsP4.Py(), higgsP4.Pz(), higgsP4.E()
+
+    if dR < 2.0: continue # we want to get rid of these results, because it is difficult to discern between the top and higgs in these cases
+
+    # Now have the topP4 and higgsP4. So we can reconstruct the Tprime quark and calculate a mass
+
     
-    
+
     del jetsP4[:]
     del fjets[:]
     del centjets[:]
     #print ievt
+
 fout.Write()
 fout.Close()
+
+# Create Mass Reconstruction Function
+# Create Boosted Mass Reconstruction Function
+# Create Chi2 Function
+# Create Boosted Chi2 Function
